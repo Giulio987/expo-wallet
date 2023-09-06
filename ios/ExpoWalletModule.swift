94e144f4ddfe226f1850fef302883913e128f2c0
Giulio_M <giulio.milani99@gmail.com>
2023-09-06 11:48:59 +0200

    iOS working

--- ios/ExpoWalletModule.swift
+++ ios/ExpoWalletModule.swift
@@ -1,43 +1,27 @@
 import ExpoModulesCore
-
+import PassKit
 public class ExpoWalletModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoWallet')` in JavaScript.
     Name("ExpoWallet")
+    AsyncFunction("addPassFromBase64") { (value: String, promise: Promise) in
+      let library = PKPassLibrary()
+      let NSData = NSData(base64Encoded: value, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ExpoWalletView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: ExpoWalletView, prop: String) in
-        print(prop)
+      if PKPassLibrary.isPassLibraryAvailable(){
+        do {
+          let pass = try PKPass(data: NSData! as Data)
+          library.addPasses([pass], withCompletionHandler: ((PKPassLibraryAddPassesStatus) -> Void)? {(status) in 
+            if status == PKPassLibraryAddPassesStatus.didAddPasses {
+              promise.resolve(true)
+            } else {
+              promise.reject(Exception(name: "E_PASS_LIBRARY_UNAVAILABLE1", description: "Pass library unavailable"))
+            }
+          })
+        } catch {
+          promise.reject(Exception(name: "E_PASS_LIBRARY_UNAVAILABLE2", description: "Pass library unavailable"))
+        }
+      } else {
+        promise.reject(Exception(name: "E_PASS_LIBRARY_UNAVAILABLE3", description: "Pass library unavailable"))
       }
     }
   }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ExpoWallet.web.ts
-// and on native platforms to ExpoWallet.ts
 import ExpoWalletModule from './ExpoWalletModule';
-import ExpoWalletView from './ExpoWalletView';
-import { ChangeEventPayload, ExpoWalletViewProps } from './ExpoWallet.types';
 
-// Get the native constant value.
-export const PI = ExpoWalletModule.PI;
-
-export function hello(): string {
-  return ExpoWalletModule.hello();
+export async function addPassFromBase64(
+  passData: string
+): Promise<boolean | Error> {
+  return await ExpoWalletModule.addPassFromBase64(passData);
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoWalletModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoWalletModule ?? NativeModulesProxy.ExpoWallet);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoWalletView, ExpoWalletViewProps, ChangeEventPayload };
